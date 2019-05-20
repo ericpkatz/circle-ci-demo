9060ab8b4d0e0d56380bc9cf6fbf840e2ee57c72
Eric P. Katz <ericpkatz@gmail.com>
2019-05-20 07:32:45 -0400

    add env file ;

--- test.spec.js
+++ test.spec.js
@@ -1,3 +1,9 @@
+try{
+  Object.assign(process.env, require('./.env.js'));
+}
+catch(ex){
+  console.log('no .env file found');
+}
 const expect = require('chai').expect;
 
 describe('Some Javascript', ()=> {
@@ -11,6 +17,11 @@ describe('Some Javascript', ()=> {
       const a = [1, 2];
       const b = [3, 4];
       expect(a.concat(b)).to.eql([1, 2, 3, 4 ]);
+    });
+  });
+  describe('an environment variable', ()=> {
+    it('FOO is BAR', ()=> {
+      expect(process.env.FOO).to.equal('BAR');
     });
   });
 });
